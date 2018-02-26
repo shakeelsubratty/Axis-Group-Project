7be2db290df171133c1f287b2312b20aeddc038a
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-02-26 21:18:10 +0000

    messing around with stuff, seems to work

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var http = require("http");
 var mongoose = require('mongoose');
+var express = require('express');
+var Promise = require('bluebird');
 const url = `mongodb://${process.env.DB_PORT_27017_TCP_ADDR}:${process.env.DB_PORT_27017_TCP_PORT}/app`
 mongoose.connect(url);
+
+var app = express();
 
 var db = mongoose.connection;
 var connected = "not yet connected";
@@ -24,12 +27,52 @@ var Idea = mongoose.model("Idea", ideaSchema);
 var test = new Idea({byUser: 'me', text: 'this is gay'});
 test.save(function(err, idea){
     if (err) return console.error(err);
+});
+var test2 = new Idea({byUser: 'you', text: 'no u'});
+test2.save(function(err, idea) {
+    if (err) return console.error(err);
 });//*/
 
-http.createServer(function(request, response) {
+findAsync = Promise.promisify(Idea.find); //should probably be something like mongoose.model.prototype.find or whatever
+
+app.get('/', function(request, response) {
     var idea = Idea.find({user:"me"});
+    var printIdeas = function(ideas) {
+        ideas.forEach(function(idea) {
+            response.write(idea.text + "\n");
+        });
+    };
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Here are the ideas:\n");
+    response.write("Here are my ideas:\n");
+    /*tasks = [];
+    tasks.push(findAsync(Idea, {byUser: 'you'}));
+    tasks.push(findAsync(Idea, {byUser: 'me'}));//*/
+    //*
+    Promise.all(
+        [
+            findAsync.call(Idea, {byUser: 'you'}),
+            findAsync.call(Idea, {byUser: 'me'})
+        ]
+    ).then(function(ideaSets) {
+        console.log("FOUNF");
+    });//*/
+    /*
+    findAsync
+        .call(Idea, {byUser: 'me'})
+        .then(printIdeas)
+        .then(function() {
+            findAsync
+                .call(Idea, {byUser: 'you'})
+                .then(printIdeas)
+                .catch(function(err) {
+                    response.write("ERROR: " + err);
+                }).finally(function() {
+                    response.end();
+                })
+            }
+        
+    );//*/
+    /*
     Idea.find({byUser: "me"}, function(err, ideas) {
         if (err) {
             response.write("ERROR: " + err);
@@ -38,6 +81,17 @@ http.createServer(function(request, response) {
         ideas.forEach(function(idea) {
             response.write(idea.text + "\n");
         });
-        response.end();
     });
-}).listen(3000);
+    response.write("Here are your ideas:\n");
+    Idea.find({byUser: "you"}, function(err, ideas) {
+        if (err) {
+            response.write("ERROR: " + err);
+            return;
+        }
+        ideas.forEach(function(idea) {
+            response.write(idea.text + "\n");
+        });
+    });//*/
+});
+
+app.listen(3000, function(){console.log("App started on port 3000");});
