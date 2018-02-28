b07228b5b907561fd89ab1e44cc0068249e0001c
Alex <franchtapia@gmail.com>
2018-02-28 19:27:35 +0000

    Add Card to Enter Workshop
    Created a Card in the center of the screen with input for the workshop id

--- src/components/enterWorkshop.js
+++ src/components/enterWorkshop.js
@@ -3,10 +3,32 @@ import React, { Component } from 'react';
 export default class EnterWorkshop extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          Enter Workshop
-        </h1>
+      <div className='main' style={{ backgroundColor: 'red' }}>
+
+			<div className='wrapper'>
+				<div className='card' style={{ width: '80%'}}>
+					<h1 className='card-header' style={{ textAlign: 'center', padding: '5%'}}>Join the workshop</h1>
+					<div className="card-body">
+						<div className='form-group'>
+							<form >
+								<label >Workshop ID:</label>
+								<input
+									className='form-control'
+									type='text'
+									name='workshopIdInput'
+									placeholder='Place your workshop id here.'
+
+								/>
+								<div className="button-box" >
+									<button type="button" className="btn btn-primary">Submit</button>
+									<button type="button" className="btn btn-primary">Cancel</button>
+								</div>
+							</form>
+						</div>
+					</div>
+
+				</div>
+			</div>
       </div>
     );
   }
