b1731a9cce088ccb75089b5f1bcee3e2c21ed716
Alex <franchtapia@gmail.com>
2018-03-05 12:37:01 +0000

    Create grid layout, action for fetching posts and component to display them
    Created action creator which passes ideas to be displayed. Created a component to display the information of each idea.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const JOIN_WORKSHOP = 'join_workshop'
+export const JOIN_WORKSHOP = 'join_workshop';
+export const FETCH_IDEAS = 'fetch_ideas';
 
 export function attemptLogIn(authIsCorrect) {
   return {
@@ -25,4 +26,21 @@ export function joinWorkshop(workshopId) {
 		type: JOIN_WORKSHOP,
 		payload: request
 	};
+}
+
+export function fetchIdeas() {
+
+	const request = [
+		{ id: 0, idea: "Elon Musk is a genious", explanation:'he has multiple successfull companies'},
+		{ id: 1, idea: "I should buy a Model S", explanation:'It is quick'},
+		{ id: 2, idea: "I should buy a Model X", explanation:'It is quick'},
+
+	];
+
+	console.log('request ==>',request);
+
+	return {
+		type: FETCH_IDEAS,
+		payloard: request
+	}
 }
--- src/containers/ideaGeneration.js
+++ src/containers/ideaGeneration.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { fetchIdeas } from '../actions';
+import { UserIdea } from '../components'
 
 class IdeaGeneration extends Component {
 
+	componentDidMount() {
+		this.props.fetchIdeas();
+	}
+
+	renderIdeas() {
+		return _.map(this.props.ideas, idea => {
+			return (
+				<UserIdea title={idea.title} description={idea.description} />
+			);
+		});
+	}
+
 	render() {
+		console.log('this.props.ideas=>',this.props.ideas)
 		return (
 			<div className='main'>
-				<div className='wrapper'>
-					<h1>IdeaGeneration</h1>
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-sm" style={{ textAlign: 'center', backgroundColor: 'yellow'}}>100%</div>
+					</div>
+
+					<div className="row">
+						<div className="col-sm-6" style={{ backgroundColor: 'red'}}>50%</div>
+						<div className="col-sm-6" style={{ backgroundColor: 'orange' }}>50%</div>
+					</div>
 				</div>
-	      </div>
+			</div>
+
+
 		);
 	}
 }
 
-export default IdeaGeneration;
+function mapStateToProps(state) {
+	return { ideas: state.ideas };
+}
+
+export default connect(mapStateToProps, {fetchIdeas})(IdeaGeneration);
