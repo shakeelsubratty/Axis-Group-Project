9b0d14f11e4598391d726969ac98efc23a5e00d8
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-03-05 11:58:45 +0000

    fixed up logging and API endpoint returns

--- backend/routes/participantRoutes.js
+++ backend/routes/participantRoutes.js
@@ -5,8 +5,11 @@ const participantModel = require('../models/participantModel');
 module.exports = function(app) {
     app.get(config.participantRoot + '/create/:name', function(req, res) {
         participantModel.createParticipant(req.params.name).then(function(newId) {
+            if (config.DEBUG) {
+                console.log("[API accessed] [participantRoute] /create/:name; created user with id: " + newId);
+            }
             res.writeHead(200, {"Content-Type": "text/plain"});
-            res.write("New user created, id is: " + newId);
+            res.write(newId);
             res.end();
         });
     });
@@ -15,9 +18,14 @@ module.exports = function(app) {
         participantModel.getParticipantName(req.params.id).then(function(name) {
             res.writeHead(200, {"Content-Type": "text/plain"});
             if (name) {
-                res.write("User found with name: " + name);
+                if (config.DEBUG) {
+                    console.log("[API accessed] [participantRoute] /view/:id; found user with name: " + name);
+                }
+                res.write(name);
             } else {
-                console.log("Failed to retrieve participant with id: " + req.params.id);
+                if (config.DEBUG) {
+                    console.log("Failed to retrieve participant with id: " + req.params.id);
+                }
                 res.write("No participant with this id");
             }
             res.end();
