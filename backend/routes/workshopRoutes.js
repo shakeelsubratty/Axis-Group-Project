b3c9967c82776062a8c678b20ad07be59347e34c
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-03-12 20:20:39 +0000

    better working workshop API and an example usage API

--- backend/routes/workshopRoutes.js
+++ backend/routes/workshopRoutes.js
@@ -4,18 +4,19 @@ const workshopModel = require('../models/workshopModel');
 const Schma = require('../models/schema');
 
 module.exports = function(app) {
-    app.get(config.workshopRoot + '/create/:type/:description', function(req, res) {
-        workshopModel.createWorkshop(req.params.type, req.params.description).then(function(newId) {
+    app.get(config.workshopRoot + '/create/:title/:description', function(req, res) {
+        workshopModel.createWorkshop(req.params.title, req.params.description).then(function(newId) {
             if (config.DEBUG) {
-                console.log("[API accessed] [workshopRoute] /create/:type/:description; created workshop with id: " + newId);
+                console.log("[API accessed] [workshopRoute] /create/:title/:description; created workshop with id: " + newId);
             }
             res.writeHead(200, {"Content-Type": "text/plain"});
-            res.write(newId.toString());
+            res.write(newId);
             res.end();
         });
     });
 
-    app.get('/test/work/', function(req, res) {
+    //TESTING - DEMO
+    app.get(config.testRoot + '/populateSomeTestUsers/', function(req, res) {
         var wshp = Schma.Workshop({title: "hello", description: "aaaaaathatssixas"});
         var usrOne = Schma.User({name: "One", workshop: wshp._id});
         var usrTwo = Schma.User({name: "Two", workshop: wshp._id});
@@ -31,30 +32,28 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/test/lau/:id', function(req, res) {
+    app.get(config.workshopRoot + '/view/:id/users', function(req, res) {
         Schma.Workshop.findByIdAsync({_id: req.params.id}).then(function(ret) {
             ret.findUsersAsync().then(function(ret) {
-                res.write("hi");
+                res.json(ret);
                 res.end();
-                console.log(ret);
             });
         });
     });
 
     app.get(config.workshopRoot + '/view/:id', function(req, res) {
-        workshopModel.getWorkshopDescription(req.params.id).then(function(description) {
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            if (description) {
+        workshopModel.getWorkshop(req.params.id).then(function(workshop) {
+            if (workshop) {
                 if (config.DEBUG) {
-                    console.log("[API accessed] [workshopRoute] /view/:id; found workshop with description: " + description);
+                    console.log("[API accessed] [workshopRoute] /view/:id; found workshop with title: " + workshop.title + " and description: " + workshop.description);
                 }
-                res.write(description);
+                res.json(workshop);
             } else {
                 if (config.DEBUG) {
                     console.log("Failed to retrieve workshop with id: " + req.params.id);
                 }
             }
-            res.end();
+            //res.end();
         });
     });
 };
