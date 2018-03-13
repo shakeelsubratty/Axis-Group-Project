6e9eb8b74236013852de34c5d2e010f43ddaa31c
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-03-13 22:56:08 +0000

    cors

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,11 @@ db.once('open', function() {
     connected = "Connected to mongo successfully!";
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
