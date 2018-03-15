4f174dbad83b2324cd16d7005d9db0687176c5a5
Lewis Brandon <lewis.brandon@kcl.ac.uk>
2018-03-15 11:04:56 +0000

    Fix deleting ideas from db

--- backend/routes/ideaRoutes.js
+++ backend/routes/ideaRoutes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
         });
     });
     app.get(config.ideaRoot + '/delete/:id', function(req, res) {
-        ideaModel.deleteIdea(req.query.id);
+        ideaModel.deleteIdea(req.params.id);
         res.sendStatus(200);
     });
 }
