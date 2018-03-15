4229941300ddb5c29d447949d7a680ac008209c6
Lewis Brandon <lewis.brandon@kcl.ac.uk>
2018-03-15 01:07:32 +0000

    Fix delete idea ID referencing

--- backend/routes/ideaRoutes.js
+++ backend/routes/ideaRoutes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
         });
     });
     app.get(config.ideaRoot + '/delete/:id', function(req, res) {
-        ideaModel.deleteIdea(id);
+        ideaModel.deleteIdea(req.query.id);
         res.sendStatus(200);
     });
 }
