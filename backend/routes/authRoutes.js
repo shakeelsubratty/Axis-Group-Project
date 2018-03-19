1a83c75163cc14972e1f7574e25f4ef29c1c7699
Lewis Brandon <lewis.brandon@kcl.ac.uk>
2018-03-19 12:32:39 +0000

    Add documentation for API routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,21 +1,44 @@
+/**
+ * authRoutes, provides the API routes to authenticate facilitators
+ * through the authModel functions.
+ */
+
+//Application requirements
 const Promise = require('bluebird');
 const config = require('../config');
 const authModel = require('../models/authModel');
 const Schema = require('../models/schema');
 
+/**
+ * authModel application routes
+ */
 module.exports = function(app) {
 
+  /**
+   * Route to create a facilitator account.
+   * @param {string} :username - The username of the account
+   * @param {string} :password - The password of the account
+   */
   app.get(config.authRoot + '/create/:username/:password', function(req, res) {
+
+    //Call the authentication model function to create a facilitator within mongodb
     authModel.createFacilitator(req.params.username, req.params.password).then(function(facilitator) {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write(facilitator);
+        res.writeHead(200, {"Content-Type": "text/plain"}); //Return 200 (OK) HTTP header
+        res.write(facilitator); //Write the model's return value to the document
         res.end();
     });
   });
 
+  /**
+   * Route to validate provided facilitator credentials.
+   * @param {string} :username - The username provided for the account
+   * @param {string} :password - The password provided for the account
+   */
   app.get(config.authRoot + '/login/:username/:password', function(req, res) {
+
+    //Call the authentication model function to authenticate facilitator credentials within mongodb
     authModel.authenticate(req.params.username, req.params.password).then(function(authenticated) {
-        res.jsonp(authenticated);
+        res.jsonp(authenticated); //Return a JSON encoded value returned by the model
         res.end();
     });
   });
--- backend/routes/participantRoutes.js
+++ backend/routes/participantRoutes.js
@@ -1,36 +1,68 @@
+/**
+ * participantRoutes, provides the API routes to create a participant
+ * and view their ideas through the participantModel functions.
+ */
+
+//Application requirements
 const Promise = require('bluebird');
 const config = require('../config');
 const participantModel = require('../models/participantModel');
 const Schma = require('../models/schema');
 
+/**
+ * participantModel application routes
+ */
 module.exports = function(app) {
-    app.get(config.participantRoot + '/create/:workshop', function(req, res) {
-        participantModel.createParticipant(req.params.workshop).then(function(newId) {
-            if (config.DEBUG) {
-                console.log("[API accessed] [participantRoute] /create/:workshop; created user with id: " + newId);
-            }
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            res.write(newId);
-            res.end();
-        });
-    });
 
-    app.get(config.participantRoot + '/view/:id/ideas', function(req, res) {
-        Schma.User.findByIdAsync(req.params.id).then(function(user) {
-            if (config.DEBUG) {
-                console.log("[API accessed] [participantRoute] /view/:id; found user: " + user);
-            }
-            if (user) {
-                user.findIdeasAsync().then(function(ideas) {
-                    if (config.DEBUG) {
-                        console.log("Found ideas for user:");
-                        console.log(ideas);
-                    }
-                    res.json(ideas);
-                });
-            } else {
-                res.sendStatus(404);
-            }
-        });
+  /**
+   * Create a new participant, linking to a workshop.
+   * @param {string} :workshop - The ID of the workshop to add the participant to
+   */
+  app.get(config.participantRoot + '/create/:workshop', function(req, res) {
+
+    //Call the participantModel to create and add the new participant to a workshop
+    participantModel.createParticipant(req.params.workshop).then(function(newId) {
+
+      //Debug console output
+      if (config.DEBUG) {
+          console.log("[API accessed] [participantRoute] /create/:workshop; created user with id: " + newId);
+      }
+      res.writeHead(200, {"Content-Type": "text/plain"}); //Return 200 (OK) HTTP header
+      res.write(newId); //Write the ID of the new participant to the document
+      res.end();
     });
+  });
+
+  /**
+   * View the ideas of a provided participant.
+   * @param {string} :id - The ID of the participant
+   */
+  app.get(config.participantRoot + '/view/:id/ideas', function(req, res) {
+
+    //Locate the user object within the schema, through the provided ID
+    Schma.User.findByIdAsync(req.params.id).then(function(user) {
+
+      //Debug console output
+      if (config.DEBUG) {
+          console.log("[API accessed] [participantRoute] /view/:id; found user: " + user);
+      }
+
+      //If the user object is found through the provided ID
+      if (user) {
+
+        //Find the ideas the participant has provided
+        user.findIdeasAsync().then(function(ideas) {
+
+          //Debug console output
+          if (config.DEBUG) {
+              console.log("Found ideas for user:");
+              console.log(ideas);
+          }
+          res.json(ideas); //Return JSON encoded value of ideas
+        });
+      } else {
+          res.sendStatus(404); //Return 404 (Not Found) status, as user not found
+      }
+    });
+  });
 };
