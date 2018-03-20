59c4c081ab6f233967a43c2e5955462bdcf2a31a
Manuel Martinez <manuel@Manuels-MacBook-Pro.local>
2018-03-20 17:38:54 +0100

    Added front end functionality to monitor when the participant switches tabs while in a workshop

--- front-end/src/containers/ideaGeneration.js
+++ front-end/src/containers/ideaGeneration.js
@@ -47,9 +47,12 @@ class IdeaGeneration extends Component {
 			this.props.fetchIdeas(this.props.wsId);
 		}
 		console.log('here with wsId->',this.props.wsId);
-		// var intervalId = setInterval(() => {
-		// 	this.props.fetchIdeas(this.props.wsId);
-		// }, 3000);
+		var intervalId = setInterval(() => {
+			if (!document.hasFocus()) {
+				// TODO: Make api call to tell the backend the user has switched tabs
+				console.log('USER SWITCHED TAB');
+			}
+		}, 1000);
 	}
 
 	componentWillUnmount(){
