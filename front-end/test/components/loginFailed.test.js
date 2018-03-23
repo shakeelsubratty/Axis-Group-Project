6e546b9f0bde7cc99ef2d4048f946e8cad1ba260
Alex <franchtapia@gmail.com>
2018-03-23 01:41:04 +0100

    1 test pass

--- front-end/test/components/loginFailed.test.js
+++ front-end/test/components/loginFailed.test.js
@@ -1,3 +1,12 @@
 import React from 'react';
-import LoginFailed from '../../src/components';
-import { shallow } from 'enzyme';
+import LoginFailed from '../../src/components/loginFailed';
+import { shallow, containsALlMatchingElements } from 'enzyme';
+
+describe("LoginFailed Component", () => {
+	it('Contains correct html tags', () => {
+		const wrapper = shallow(<LoginFailed />);
+		const text = wrapper.find('h2').text();
+		expect(text).toEqual('Oops...It looks like you are not logged in...')
+	})
+
+})
