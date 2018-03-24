23efaf73ec565ff6103a5b083a62f77f4d93df5f
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-03-24 16:34:36 +0000

    switched docker to host network mode

--- backend/config.js
+++ backend/config.js
@@ -4,11 +4,11 @@
  */
 module.exports = {
     DEBUG: true, //currently used to spew a bunch of diagnostics into the console
-    mongoUrl: `mongodb://${process.env.DB_PORT_27017_TCP_ADDR}:${process.env.DB_PORT_27017_TCP_PORT}/app`, //URL to connect to mongodb
+    mongoUrl: "mongodb://localhost:27017/app", //URL to connect to mongodb
     participantRoot: "/participant", //routes for everything participant-related
     workshopRoot: "/workshop", //routes for everything workshop-related
     ideaRoot: "/idea",
     authRoot: "/auth", //routes for everything workshop-related
     analysisRoot: "/analysis",
-    aiUrl: "http://172.17.0.1:25565"
+    aiUrl: "http://localhost:25565"
 }
