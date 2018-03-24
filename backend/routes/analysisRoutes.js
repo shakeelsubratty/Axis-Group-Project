1823634b21123ccc4e5d6256a54b54bbcea5621b
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-03-24 19:27:57 +0000

    added boilerplate for wordcloud and added mock return

--- backend/routes/analysisRoutes.js
+++ backend/routes/analysisRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     app.get(config.analysisRoot + '/userengagement/:workshopId', function(req, res) {
         analysisModel.getUsersAndIdeaText(req.params.workshopId).then(function(ret) {
             if (config.DEBUG) {
-                console.log("[API accessed] [analysisRoute] /userengagement/:workshopId, got the following engagement: " + JSON.stringify(ret));
+                console.log("[API accessed] [analysisRoute] /userengagement/:workshopId, currently aggregated the following user data: " + JSON.stringify(ret));
             }
             /*********TODO: remove mock and reinstate proper API call***********/
             var mockRet = {
@@ -29,6 +29,21 @@ module.exports = function(app) {
                     res.json(body);
                 }
             );*/
+        });
+    });
+
+    app.get(config.analysisRoot + '/wordcloud/:workshopId', function(req, res) {
+        analysisModel.getUsersAndIdeaText(req.params.workshopId).then(function(ret) {
+            if (config.DEBUG) {
+                console.log("[API accessed] [analysisRoute] /wordcloud/:workshopId, currently aggregated the following user data: " + JSON.stringify(ret));
+            }
+            var mockRet = [
+                { "name": "Potato", "count": 1, "colour": "#00ff00", "sentiment": 1 },
+                { "name": "Ireland", "count": 1, "colour": "#ff0000", "sentiment": -1 },
+                { "name": "Manuel", "count": 1, "colour": "#f0f000", "sentiment": 0.5 },
+                { "name": "Alex", "count": 2, "colour": "#0f0f00", "sentiment": -0.8 }
+            ];
+            res.json(mockRet);
         });
     });
 }
