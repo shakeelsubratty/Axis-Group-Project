5d0574eba9575db3bb5884e4023100b43589ee80
shakeelsubratty <shakeel.subratty@gmail.com>
2018-03-25 21:23:27 +0100

    Implement gson for userengagement

--- ai/src/main/java/analysis/AIController.java
+++ ai/src/main/java/analysis/AIController.java
@@ -16,6 +16,8 @@ import org.springframework.http.HttpEntity;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.google.gson.*;
+
 import analysis.RepetitionGrouper;
 import analysis.UserEngagementCalculator;
 import data.Response;
@@ -37,22 +39,48 @@ public class AIController {
 //        return new ResponseEntity<UserEngagementResponse>(u, HttpStatus.OK);
 //    }
     @RequestMapping(value = "/userengagement", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Participant> userEngagement(@RequestBody List<Participant> participants)
+    @ResponseBody
+    public String userEngagement(HttpEntity<String> s)
     {
-//        String json = s.getBody();
-//
-//        return json;
+        String json = s.getBody();
 
-        return new ResponseEntity<Participant>(participants.get(0),HttpStatus.OK);
-        
+        Gson g = new Gson();
+
+        JsonParser parser = new JsonParser();
+
+        JsonArray participants = parser.parse(json).getAsJsonArray();
+
+        List<Participant> participants = new ArrayList<Participant>;
+
+        for(JsonElement e : participants)
+        {
+            List<Response> responses = new ArrayList<>();
+
+            for(JsonElement idea : e.get(1).getAsJsonArray())
+            {
+                Response r = new Response(g.fromJson(idea.get(0),String.class),g.fromJson(idea.get(1),String.class));
+                responses.add(r);
+            }
+
+            Participant participant = new Participant(g.fromJson(e.get(0),String.class),responses);
+
+            participants.add(participant);
+        }
+
+        UserEngagementCalculator uec = new UserEngagementCalculator(participants);
+
+        uec.calculateLevel();
+
+        Collection<Double> results = uec.returnAverageArr();
+
+        String ret = g.toJson(results);
+
+        return ret;
+
 //        return new ResponseEntity<String>("Hi there! Participant: ",HttpStatus.OK);
 //        System.out.println(participants.get(0).getID());
 //
-//        UserEngagementCalculator uec = new UserEngagementCalculator(participants);
 //
-//        uec.calculateLevel();
-//
-//        UserEngagementResponse u = new UserEngagementResponse(uec.returnAverageArr());
 //        System.out.println("UserEngagement: Returning" + u.getEngagementResponse());
 //        return new ResponseEntity<List<Double>>(u.getEngagementResponse(), HttpStatus.OK);
     }
