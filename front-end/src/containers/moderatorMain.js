ed6d5c7ca7ef87ab7b9db3e70f18835ba7666750
Alex <franchtapia@gmail.com>
2018-03-28 18:42:34 +0200

    Refactor moderatorMain

--- front-end/src/containers/moderatorMain.js
+++ front-end/src/containers/moderatorMain.js
@@ -3,169 +3,187 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import { WorkshopIdea, LoadingScreen, WordCloud } from '../components';
-import { deactivateWorkshop, getWorkshopInfo, setWorkshopTo, attemptLogIn, logOut, fetchAllIdeas, getUserEngagement, getWordCloudData } from '../actions';
-
+import { deactivateWorkshop,
+			getWorkshopInfo,
+			setWorkshopTo,
+			attemptLogIn,
+			logOut,
+			fetchAllIdeas,
+			getUserEngagement,
+			getWordCloudData } from '../actions';
+/*
+Class which presents the ai and ideas to the facilitator. Has all the analytics
+and Ideas in the workshopId
+*/
 export class ModeratorMain extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+	constructor(props) {
+   	super(props);
+    	this.state = {
       isLogged: false,
-    };
-    var intervalWsId = '';
+   };
+   let intervalWsId = '';
   }
 
-  componentWillMount(){
-    if (sessionStorage.getItem('wsId') == '') {
-      if (!this.props.wsId) {
-        // If you try to get here from login without creating a workshop
-        this.props.history.push('/create-workshop')
-      }
-    } else {
-      this.props.setWorkshopTo(sessionStorage.getItem('wsId'))
-      this.setState({isLogged:true})
-    } // If you refresh
+	// All functions called before the component renders.
+	componentWillMount() {
+		if (sessionStorage.getItem('wsId') == '') {
+			if (!this.props.wsId) {
+        	// If you try to get here from login without creating a workshop
+			this.props.history.push('/create-workshop')
+      	}
+    	} else {
+      	this.props.setWorkshopTo(sessionStorage.getItem('wsId'))
+      	this.setState({isLogged:true})
+    	}
+		// If you refresh
+    	if ((sessionStorage.getItem('usrn') == '' || sessionStorage.getItem('pass') == '')) {
+      	if (!this.props.isLogged) {
+				// If you try to bypass login
+				this.props.history.push('/login-failed');
+      	}
+    	} else {
 
-    if ((sessionStorage.getItem('usrn') == '' || sessionStorage.getItem('pass') == '')) {
-      if (!this.props.isLogged) {
-        // If you try to bypass login
-        this.props.history.push('/login-failed');
-      }
-    } else {
-      const usrn = sessionStorage.getItem('usrn');
-      const pass = sessionStorage.getItem('pass');
-      this.props.attemptLogIn(usrn,pass, ()=>{
-        if (!this.props.isLogged) {
-          // If you try to inject invalid login credentials
-          this.props.history.push('/login-failed');
-        }
-      });
-    }
-  }
+			const usrn = sessionStorage.getItem('usrn');
+      	const pass = sessionStorage.getItem('pass');
+      	this.props.attemptLogIn(usrn,pass, ()=>{
 
-  componentDidMount(){
-    if (this.props.wsId != '') {
-      this.props.getWorkshopInfo(this.props.wsId);
-      this.props.fetchAllIdeas(this.props.wsId);
-      this.props.getWordCloudData(this.props.wsId);
-    }
-    if (this.state.isLogged) {
-      window.intervalWsId = setInterval(() => {
-        this.props.fetchAllIdeas(this.props.wsId);
-        this.props.getUserEngagement(this.props.wsId);
-        this.props.getWordCloudData(this.props.wsId);
-      }, 4000);
-    }
-  }
+				if (!this.props.isLogged) {
+          	// If you try to inject invalid login credentials
+          	this.props.history.push('/login-failed');
+		 	}
+      	});
+    	}
+	}
 
-  componentWillReceiveProps(nextProps){
-    if (this.props.wsId != nextProps.wsId) {
-      this.props.getWorkshopInfo(nextProps.wsId);
-      this.props.fetchAllIdeas(this.props.wsId);
-    }
-  }
+	// All functions called after the component renders.
+	componentDidMount() {
+   	if (this.props.wsId != '') {
+      	this.props.getWorkshopInfo(this.props.wsId);
+      	this.props.fetchAllIdeas(this.props.wsId);
+      	this.props.getWordCloudData(this.props.wsId);
+    	}
 
-  componentWillUnmount(){
-    clearInterval(window.intervalWsId);
-  }
+		if (this.state.isLogged) {
+      	window.intervalWsId = setInterval(() => {
+        		this.props.fetchAllIdeas(this.props.wsId);
+        		this.props.getUserEngagement(this.props.wsId);
+        		this.props.getWordCloudData(this.props.wsId);
+      	}, 4000);
+    	}
+  	}
 
-  renderIdeas() {
+	componentWillReceiveProps(nextProps) {
+		if (this.props.wsId != nextProps.wsId) {
+      	this.props.getWorkshopInfo(nextProps.wsId);
+      	this.props.fetchAllIdeas(this.props.wsId);
+    	}
+  	}
+
+	// Functions before another screen is rendered.
+	componentWillUnmount(){
+   	clearInterval(window.intervalWsId);
+	}
+
+	// Function which renders all the ideas in a list.
+	renderIdeas() {
 		return Object.keys(this.props.wsIdeas).map((item)=>{
 			return (
 				<div key={this.props.wsIdeas[item]._id}>
 					<WorkshopIdea
 						id = {this.props.wsIdeas[item]._id}
 						title={this.props.wsIdeas[item].title}
-            data={this.props.wordCloudData}
+            		data={this.props.wordCloudData}
 					>
-            {this.props.wsIdeas[item].description}
+            	{this.props.wsIdeas[item].description}
 					</WorkshopIdea>
 				</div>
 			)
 		});
 	}
 
-  renderUserEngagementData(){
-    if (!this.props.userEngagement || this.props.userEngagement.status == 500) {
-      return(
-        <div className='card flexColumnCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important', padding:'2%', alignItems:'stretch'}}>
-          <h5 style={{textAlign:'center'}}>Loading...</h5>
-        </div>
-      );
-    } else {
-      let userEngagementColor = 'black';
+	// Renders data displayed for user engagement analytic.
+	renderUserEngagementData() {
+   	if (!this.props.userEngagement || this.props.userEngagement.status == 500) {
+      	return(
+        		<div className='card flexColumnCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important', padding:'2%', alignItems:'stretch'}}>
+          		<h5 style={{textAlign:'center'}}>Loading...</h5>
+        		</div>
+      	);
+    	} else {
+      	let userEngagementColor = 'black';
 
-      if (this.props.userEngagement.overallEngagement > 0.7) {
-        userEngagementColor = 'green';
-      } else if (this.props.userEngagement.overallEngagement < 0.4) {
-        userEngagementColor = 'red';
-      }
+      	if (this.props.userEngagement.overallEngagement > 0.7) {
+        		userEngagementColor = 'green';
+      	} else if (this.props.userEngagement.overallEngagement < 0.4) {
+        		userEngagementColor = 'red';
+      	}
 
-      return(
-        <div className='card flexRowCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important', padding:'3%', alignItems:'stretch'}}>
-          <div className='flexColumnCenter' style={{flex:1, alignItems:'stretch', borderRight:'1px solid black '}}>
-            <div style={{flex:1, textAlign:'left', display:'flex'}}>
-              <span style={{flex:3, fontSize:'11pt', textAlign:'left'}}>Really Engaged:</span>
-              <span style={{flex:1}}>{this.props.userEngagement[3] * 100 + '%'}</span>
-            </div>
-            <div style={{flex:1, textAlign:'left', display:'flex'}}>
-              <span style={{flex:3,fontSize:'11pt', textAlign:'left'}}>Engaged:</span>
-              <span style={{flex:1}}>{this.props.userEngagement[2] * 100 + '%'}</span>
-            </div>
-            <div style={{flex:1, textAlign:'left', display:'flex'}}>
-              <span style={{flex:3,fontSize:'11pt', textAlign:'left'}}>Unengaged:</span>
-              <span style={{flex:1}}>{this.props.userEngagement[1] * 100 + '%'}</span>
-            </div>
-            <div style={{flex:1, textAlign:'left', display:'flex'}}>
-              <span style={{flex:3,fontSize:'11pt', textAlign:'left'}}>Really Unengaged:</span>
-              <span style={{flex:1}}>{this.props.userEngagement[0] * 100 + '%'}</span>
-            </div>
-          </div>
-          <div className='flexColumnCenter' style={{flex:1}}>
-            <h3 style={{color:userEngagementColor}}>{this.props.userEngagement[4] * 100 + '%'}</h3>
-            <h5>Overall</h5>
-          </div>
-        </div>
-      );
-    }
-  }
-
-  renderWordCloud(){
-    if (!this.props.wordCloudData || _.isEmpty(this.props.wordCloudData) ) {
-      return(
-        <h5 style={{textAlign:'center'}}>Loading...</h5>
-      );
-    } else {
-      return(
-        <WordCloud className='tag-cloud' data={this.props.wordCloudData}/>
+      	return(
+        		<div className='card flexRowCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important', padding:'3%', alignItems:'stretch'}}>
+          		<div className='flexColumnCenter' style={{flex:1, alignItems:'stretch', borderRight:'1px solid black '}}>
+            		<div style={{flex:1, textAlign:'left', display:'flex'}}>
+		              <span style={{flex:3, fontSize:'11pt', textAlign:'left'}}>Really Engaged:</span>
+		              <span style={{flex:1}}>{this.props.userEngagement[3] * 100 + '%'}</span>
+            		</div>
+            		<div style={{flex:1, textAlign:'left', display:'flex'}}>
+              			<span style={{flex:3,fontSize:'11pt', textAlign:'left'}}>Engaged:</span>
+              			<span style={{flex:1}}>{this.props.userEngagement[2] * 100 + '%'}</span>
+            		</div>
+            		<div style={{flex:1, textAlign:'left', display:'flex'}}>
+              			<span style={{flex:3,fontSize:'11pt', textAlign:'left'}}>Unengaged:</span>
+              			<span style={{flex:1}}>{this.props.userEngagement[1] * 100 + '%'}</span>
+            		</div>
+            		<div style={{flex:1, textAlign:'left', display:'flex'}}>
+              			<span style={{flex:3,fontSize:'11pt', textAlign:'left'}}>Really Unengaged:</span>
+              			<span style={{flex:1}}>{this.props.userEngagement[0] * 100 + '%'}</span>
+            		</div>
+          		</div>
+          		<div className='flexColumnCenter' style={{flex:1}}>
+            	<h3 style={{color:userEngagementColor}}>{this.props.userEngagement[4] * 100 + '%'}</h3>
+            	<h5>Overall</h5>
+          		</div>
+        	</div>
       );
     }
   }
 
+	// Renders word cloud analytic.
+	renderWordCloud() {
+   	if (!this.props.wordCloudData || _.isEmpty(this.props.wordCloudData) ) {
+      	return(
+        		<h5 style={{textAlign:'center'}}>Loading...</h5>
+      	);
+    	} else {
+      	return(
+        		<WordCloud className='tag-cloud' data={this.props.wordCloudData}/>
+      	);
+   	}
+  	}
 
-  renderDataBox(){
+	// Renders data box with information about the workshop.
+	renderDataBox() {
+   	return(
+      	<div className='card card-big dataBox'>
+	        	<div className='card flexColumnCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important'}}>
+	         	{this.props.wsInfo.description}
+	        	</div>
+	        		{this.renderUserEngagementData()}
+	        	<div className='card flexColumnCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important', padding:'3%'}}>
+	          	{this.renderWordCloud()}
+	        	</div>
+      	</div>
+   	);
+  	}
 
-    return(
-      <div className='card card-big dataBox'>
-        <div className='card flexColumnCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important'}}>
-          {this.props.wsInfo.description}
-        </div>
-        {this.renderUserEngagementData()}
-        <div className='card flexColumnCenter' style={{flex:1, backgroundColor:'#f5f5f5 !important', padding:'3%'}}>
-          {this.renderWordCloud()}
-        </div>
-      </div>
-    );
-  }
-
-  renderIdeaPanel(){
-    return(
-      <div className='card card-big' style={{flex:1,borderRadius:0,borderBottom:'none',marginBottom:0,paddingBottom:'2%'}}>
-        <h3 className='card-title' style={{textAlign:'left', marginTop:'2%'}}><u>{this.props.wsTitle}</u></h3>
-        <div className='card-body' style={{flex:6,marginTop:'2%', alignItems:'stretch', overflowY:'scroll'}}>
-          <div style={{flex:1}}>
-            {this.renderIdeas()}
-          </div>
-        </div>
+	renderIdeaPanel(){
+		return(
+      	<div className='card card-big' style={{flex:1,borderRadius:0,borderBottom:'none',marginBottom:0,paddingBottom:'2%'}}>
+        		<h3 className='card-title' style={{textAlign:'left', marginTop:'2%'}}><u>{this.props.wsTitle}</u></h3>
+        		<div className='card-body' style={{flex:6,marginTop:'2%', alignItems:'stretch', overflowY:'scroll'}}>
+          		<div style={{flex:1}}>
+            		{this.renderIdeas()}
+          		</div>
+        		</div>
         <div
           className='flexRowCenter card'
           style={{backgroundColor:'#e8edf4',border:'none', marginTop:'2%', alignItems:'flex-end'}}
@@ -214,27 +232,28 @@ export class ModeratorMain extends Component {
   }
 }
 
+// Redux methods to manage global state of the app and actions.
 function mapStateToProps(state){
-  return {
-    isLogged: state.app.isLogged,
-    wsId: state.app.wsId,
-    wsInfo: state.app.wsInfo,
-    wsTitle: state.app.wsInfo.title,
-    wsIdeas: state.app.wsIdeas,
-    userEngagement: state.app.userEngagement,
-    wordCloudData: state.app.wordCloudData
-  };
+ 	return {
+   	isLogged: state.app.isLogged,
+    	wsId: state.app.wsId,
+	   wsInfo: state.app.wsInfo,
+	   wsTitle: state.app.wsInfo.title,
+	   wsIdeas: state.app.wsIdeas,
+	   userEngagement: state.app.userEngagement,
+	   wordCloudData: state.app.wordCloudData
+  	};
 }
 
 const mapDispatchToProps = {
-  getWorkshopInfo,
-  attemptLogIn,
-  logOut,
-  setWorkshopTo,
-  fetchAllIdeas,
-  getUserEngagement,
-  getWordCloudData,
-  deactivateWorkshop
+	getWorkshopInfo,
+	attemptLogIn,
+	logOut,
+	setWorkshopTo,
+	fetchAllIdeas,
+	getUserEngagement,
+	getWordCloudData,
+	deactivateWorkshop
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModeratorMain);
