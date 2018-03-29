30e8d4c980a2179376c4e223a7fe0cbc0da42da4
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2018-03-29 14:04:10 +0100

    fixed tests

--- backend/test/testConfig.js
+++ backend/test/testConfig.js
@@ -1,6 +1,5 @@
 module.exports = {
     DEBUG: false,
-    mongoUrl: `mongodb://${process.env.DB_PORT_27017_TCP_ADDR}:${process.env.DB_PORT_27017_TCP_PORT}/app`, //URL to connect to mongodb
     participantRoot: '/participant',
     workshopRoot: '/workshop',
     ideaRoot: '/idea',
