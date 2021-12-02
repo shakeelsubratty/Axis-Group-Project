2432bb2f89fc23f572b8b599883a6d040eb5ffe6
shakeelsubratty <shakeelsubratty@Shakeels-MacBook-Pro.local>
2021-12-02 19:07:29 +0000

    updated configs

--- backend/config.js
+++ backend/config.js
@@ -4,11 +4,11 @@
  */
 module.exports = {
     DEBUG: true, //currently used to spew a bunch of diagnostics into the console
-    mongoUrl: `mongodb://localhost:27017/app`, //URL to connect to mongodb
+    mongoUrl: `mongodb://db:27017/app`, //URL to connect to mongodb
     participantRoot: "/participant", //routes for everything participant-related
     workshopRoot: "/workshop", //routes for everything workshop-related
     ideaRoot: "/idea",
     authRoot: "/auth", //routes for everything workshop-related
     analysisRoot: "/analysis",
-    aiUrl: "http://localhost:25565"  //GCP
+    aiUrl: "http://host.docker.internal:25565"  //GCP
 }
